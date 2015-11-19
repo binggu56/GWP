9244c556c5a86fa8e64c627ea7e9c87aaaa8a651
Bing Gu <bing@asp.chem.sc.edu>
2015-11-19 16:50:26 -0500

    delete redundant files

--- lqf7/mod.f
+++
@@ -1,8 +0,0 @@
-        module cdat
-        real*8,public,parameter :: PI=4.d0*atan(1.0)
-        integer,public,parameter :: Ntraj = 12, Np = 1500
-C        Integer,parameter::NI=512,NJ=512,N2=NI*NJ
-        complex*16,public,parameter::im=(0d0,1d0)
-        save
-        end module cdat
-
